b1f9d428fb0074877a26a53b5e24f6994813e99f
Tuấn Ngọc <ngoctuan186205@gmail.com>
2026-06-27 18:31:49 +0700

    first commit

--- src/Main.java
+++ src/Main.java
@@ -8,46 +8,20 @@ public class Main {
         String choice = "1";
 //
         while (choice.equals("1")) {
-            System.out.println("\nNhập vào 3 cạnh của tam giác\n");
-            int canh1,canh2,canh3;
-            System.out.println("Cạnh thứ nhất: ");
+
             Scanner sc = new Scanner(System.in);
-            canh1 = Integer.parseInt(sc.nextLine());
-            System.out.println("Cạnh thứ hai: ");
-            canh2 = Integer.parseInt(sc.nextLine());
-            System.out.println("Cạnh thứ ba: ");
-            canh3 = Integer.parseInt(sc.nextLine());
-            int a = canh1*canh1 + canh2*canh2;
-            int b = canh1*canh1 + canh3*canh3;
-            int c = canh3*canh3 + canh2*canh2;
+            System.out.println("Nhập vào số nguyên N: ");
+            int n = Integer.parseInt(sc.nextLine());
+            if (n<0) n= n*(-1);
+            int soDu, soChia,dem = 0,tong=0;
+            do {
+                soChia = n/10;
+                soDu = n%10;
+                tong = tong + soDu;
+                n=n/10;
+            }while (soChia > 0);
+            System.out.printf("Tổng các chữ số là: %d",tong);
 
-            if(canh1 <=0 || canh2 <=0 ||canh3 <=0)
-            {
-
-                System.out.println("Cạnh không được nhỏ hơn hoặc bằng 0");
-
-            }
-            else if (canh1+canh2<= canh3 || canh2+canh3<=canh1 || canh1+canh3 <= canh2) {
-                System.out.println("Không phải 3 cạnh của 1 tam giác");
-
-            }
-            else if(canh1 == canh2 && canh3 == canh1)
-            {
-                System.out.println("Đây là 3 cạnh của tam giác đều");
-
-            }
-
-             else if(canh1 == canh2 || canh3 == canh1 || canh3 == canh2)
-            {
-                System.out.println("Đây là 3 cạnh của tam giác cân\n");
-
-            }
-            else if(a == canh3*canh3 || b == canh2*canh2 || c == canh1*canh1)
-            {
-                System.out.println("Đây là 3 cạnh của tam giác vuông");
-
-            }
-            else System.out.println("Đây là tam giác thường");
             System.out.println("\nBạn có muốn nhập lại không? ");
             System.out.println("nếu có ấn phím 1, nếu không ấn bất kì để thoát! ");
             choice = sc.nextLine();
